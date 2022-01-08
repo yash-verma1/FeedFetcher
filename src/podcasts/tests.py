1b3d4454fe4d22f3f81ae655a2162331b73b784b
Yash Verma <yashv9119@gmail.com>
2022-01-08 10:25:59 +0530

    Added tests for podcasts app models

--- src/podcasts/tests.py
+++ src/podcasts/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Episode
 
-# Create your tests here.
+class PodCastsTests(TestCase):
+    def setUp(self):
+        self.episode = Episode.objects.create(
+            podcast_title="Podcast 1",
+            description="lorem ipsum hakuna matata cheese burger",
+            pub_date=timezone.now(),
+            url="https://podcasts.com",
+            image="https://podcasts.image.com",
+            podcast_name="Podcast Name",
+            guid="del12o9123-124-asd123512"
+        )
+
+    def test_episode_content(self):
+        self.assertEqual(self.episode.description, "lorem ipsum hakuna matata cheese burger")
+        self.assertEqual(self.episode.url, "https://podcasts.com")
+        self.assertEqual(self.episode.guid, "del12o9123-124-asd123512")
+
+    def test_episode_str_representation(self):
+        self.assertEqual(str(self.episode), "Podcast Name: Podcast 1")
