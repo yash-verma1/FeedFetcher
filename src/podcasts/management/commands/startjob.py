327c6b722ffff9d82905f75f9421d88319041174
Yash Verma <yashv9119@gmail.com>
2022-01-14 02:52:27 +0530

    Added another podcast feed, changes in custom command behavior

--- src/podcasts/management/commands/startjob.py
+++ src/podcasts/management/commands/startjob.py
@@ -3,21 +3,40 @@ import feedparser
 from dateutil import parser
 from podcasts.models import Episode
 
+
+def save_new_episode(feed):
+    """
+    Saves new episodes
+    """
+    # feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid):
+            episode = Episode(
+                podcast_title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+def fetch_cyrusSays():
+    """ Fetches new shows from JRE """
+    _feed = feedparser.parse("http://cyrussays.ivm.libsynpro.com/rss")
+    save_new_episode(_feed)
+
+
+def fetch_realPython():
+    _feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
+    save_new_episode(_feed)
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        feed = feedparser.parse("https://realpython.com/podcasts/rpp/feed")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
-
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid):
-                episode = Episode(
-                    podcast_title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+        fetch_cyrusSays()
+        fetch_realPython()
